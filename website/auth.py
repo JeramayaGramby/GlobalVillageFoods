56e116f4de84192cfb90d3d5aa3d1c8addcd2c15
Jeramaya Gramby <jeramayagramby@gmail.com>
2022-12-25 13:41:56 -0500

    Add adminpage.html plus README.txt revisions

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth',__name__)
 
-'''Configuring the login page, logout page and signup page'''
+'''Configuring the login page, logout page, signup page, '''
 
 '''Login Page'''
 @auth.route('/login')
@@ -25,5 +25,13 @@ def logout():
 def sign_up():
     return render_template("sign_up.html",
         title = 'Login',
+        user = 'user',
+        posts = 'posts')
+
+'''Admin Page'''
+@auth.route('/restricted-access')
+def admin():
+    return render_template("adminpage.html",
+        title = 'Admin_Page',
         user = 'user',
         posts = 'posts')
